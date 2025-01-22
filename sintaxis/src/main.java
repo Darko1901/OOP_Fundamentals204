21ddb2c3390514f42e3f418a4b08119d5902c88d
Ricardo Méndez Rodríguez <123046770@upq.edu.mx>
2025-01-22 14:43:55 -0600

    Jugamos un poco con las cadenas de caracteres (String)

--- sintaxis/src/main.java
+++ sintaxis/src/main.java
@@ -1,5 +1,20 @@
+//IMPORTACIONES
+
+//CLASE PRINCIPAL
 public class main {
+    //METODO PRINCIPAL
     public static void main(String[] args){
-        System.out.println("Ricardo Mendez");
+        System.out.println("Hello, Ricardo Mendez!");
+        
+        String cadenas = "Ricardo" + " Mendez" + " Rodriguez";
+        System.out.println(cadenas);
+        System.out.println("La longitud es de: " + cadenas.length());
+
+        // Extraer caracteres de la cadena en un rango dado
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+
+        
     }
 }
